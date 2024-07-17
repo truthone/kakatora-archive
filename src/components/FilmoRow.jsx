28262d7ff8ed36b4779484be0882ecb78ff172a3
ja.han <truth.h406@gmail.com>
2024-07-18 02:17:22 +0900

    update

---
+++ src/components/FilmoRow.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Box, Heading, Flex, AspectRatio, Card, Text } from '@radix-ui/themes';
+
+function FilmoRow({ title, contents }) {
+  return (
+    <Box my="4">
+      <Heading size="6" mb="4" ml="6">{title}</Heading>
+      <Flex gap="3" px="6" style={{ overflowX: 'auto' }}>
+        {contents.map((content, id) => (
+          <Card key={id} style={{ width: '180px', flexShrink: 0 }}>
+            <AspectRatio ratio={2/3} style={{padding: '0'}}>
+              <img 
+                src={content.imgUrl} 
+                alt={content.title} 
+                style={{ 
+                  objectFit: 'cover', 
+                  width: '100%', 
+                  height: '100%',
+                  borderTopLeftRadius: 'var(--radius-2)',
+                  borderTopRightRadius: 'var(--radius-2)'
+                }}
+              />
+            </AspectRatio>
+            <Box p='2'>
+              <Text size="1" style={{ whiteSpace: 'break-spaces' }}>
+                {content.title}
+              </Text>
+            </Box>
+          </Card>
+        ))}
+      </Flex>
+    </Box>
+  );
+}
+
+export default FilmoRow;
