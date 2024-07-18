aa604f2875cc810fcf05d323cfc1e6e27b0ed00e
ja.han <truth.h406@gmail.com>
2024-07-19 02:20:35 +0900

    update: img

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import Hero from './Hero';
 import moviesData from '../data/moviesData.json';
-import filmoData from '../data/filmoData.json'
 import filmoDataByYear from '../data/filmoDataByYear.json'
-import { Container, Separator, Flex, Card } from '@radix-ui/themes';
+import { Container, Separator } from '@radix-ui/themes';
 import FilmoSection from './FilmoSection';
-import TwitterEmbed from './TwitterEmbed';
 
 function Home() {
   return (
     <Container>
       <Hero movie={moviesData.featuredMovie} />
-      <Flex>
+      {/* <Flex>
         <Card style={{ width: '300px', flexShrink: 0 }}><TwitterEmbed  /></Card>
-      </Flex>
-      <Separator orientation="horizontal" size="4" />
+      </Flex> */}
+      {/* <Separator orientation="horizontal" size="4" /> */}
       <FilmoSection data={filmoDataByYear.filmo_data_by_year} />
     </Container>
   );
