6b96191a9a7f7c0376ebbe321a05e283725a0252
한정아 <truth.h406@gmail.com>
2024-07-18 17:27:45 +0900

    udpate: 필모 전체 리스트

--- src/components/FilmoSection.jsx
+++ src/components/FilmoSection.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Box, Heading } from '@radix-ui/themes';
-import FilmoRow from './FilmoRow';  // FilmoRow 컴포넌트를 import 합니다.
+import FilmoRow from './FilmoRow';
 
 function FilmoSection({ data }) {
   return (
     <Box>
       {data.map((yearData, index) => (
         <Box key={index} my="6">
-          <Heading size="7" mb="4">{yearData.year}</Heading>
-          <FilmoRow title="드라마" contents={yearData.dramas} />
+          <Heading size="7" mb="4">{yearData?.year}</Heading>
+          {yearData?.movies && yearData.movies.length > 0 && (
+            <FilmoRow title="영화" contents={yearData?.movies} />
+          )}
+          {yearData?.dramas && yearData.dramas.length > 0 && (
+            <FilmoRow title="드라마" contents={yearData?.dramas} />
+          )}
+          {yearData?.musicals && yearData.musicals.length > 0 && (
+            <FilmoRow title="연극" contents={yearData?.musicals} />
+          )}
+          {yearData?.tv_appearances && yearData.tv_appearances.length > 0 && (
+            <FilmoRow title="예능" contents={yearData?.tv_appearances} />
+          )}
         </Box>
       ))}
     </Box>
