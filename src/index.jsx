be2abb23736b197571fb9cb1d4dfba2607476e19
ja.han <truth.h406@gmail.com>
2024-07-20 10:57:18 +0900

    update

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import '@radix-ui/themes/styles.css';
 
+if (process.env.NODE_ENV === 'production') {
+  ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
