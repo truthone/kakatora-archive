82c9d2c1acb2c4e753dcceda1f5644a1d2dc586d
한정아 <32234327+truthone@users.noreply.github.com>
2024-07-20 11:55:52 +0900

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import '@radix-ui/themes/styles.css';
+import ReactGA from 'react-ga';
 
 if (process.env.NODE_ENV === 'production') {
   ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
