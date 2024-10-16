041ff53b514ee01c6e151df000344b71af7d9717
ja.han <truth.h406@gmail.com>
2024-10-16 19:21:09 +0900

    update: css

--- components/GridImageItem.jsx
+++ components/GridImageItem.jsx
@@ -1,10 +1,18 @@
-'use client'
+'use client';
 import React, { useState, useRef } from 'react';
 import * as VisuallyHidden from '@radix-ui/react-visually-hidden';
 import * as Dialog from '@radix-ui/react-dialog';
 import styled from 'styled-components';
 import { Cross2Icon } from '@radix-ui/react-icons';
-import { AspectRatio, Box, Text, Flex, Skeleton, Button } from '@radix-ui/themes';
+import {
+  AspectRatio,
+  Box,
+  Text,
+  Flex,
+  Skeleton,
+  Button,
+  Card,
+} from '@radix-ui/themes';
 import Image from 'next/image';
 
 const Overlay = styled(Dialog.Overlay)`
@@ -14,8 +22,12 @@ const Overlay = styled(Dialog.Overlay)`
   animation: overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1);
 
   @keyframes overlayShow {
-    from { opacity: 0; }
-    to { opacity: 1; }
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
   }
 `;
 
@@ -33,8 +45,12 @@ const Content = styled(Dialog.Content)`
   animation: contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1);
 
   @keyframes contentShow {
-    from { opacity: 0; }
-    to { opacity: 1; }
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
   }
 `;
 const CloseButton = styled(Button)`
@@ -52,19 +68,18 @@ const CloseButton = styled(Button)`
   border: none;
   background-color: transparent;
   z-index: 100;
-  
-  &:hover { 
-    background-color: rgba(0, 0, 0, 0.1); 
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.1);
   }
 `;
-
 
 export default function GridImageItem({ filename, episode, index, title }) {
   const [naturalWidth, setNaturalWidth] = useState(null);
   const [naturalHeight, setNaturalHeight] = useState(null);
   const containerRef = useRef(null);
 
-  function handleImgHeight(img){
+  function handleImgHeight(img) {
     const { naturalWidth, naturalHeight } = img;
     setNaturalWidth(naturalWidth);
     setNaturalHeight(naturalHeight);
@@ -73,65 +88,88 @@ export default function GridImageItem({ filename, episode, index, title }) {
   return (
     <Dialog.Root>
       <Dialog.Trigger asChild>
-        <Flex direction="column" p="2" align="center" style={{cursor: 'pointer', border: "solid 0.5px #808080", borderRadius: "5px", justifyContent: "space-around"}}>
+        <Card
+          display="flex"
+          direction="column"
+          align="center"
+          style={{
+            cursor: 'pointer',
+            justifyContent: 'space-around',
+            height: 'fit-content',
+          }}
+          className="item"
+          variant="surface"
+        >
           <AspectRatio ratio={1 / 1}>
             <Skeleton loading="true" width="200" height="200">
               <Image
                 src={`/images/tv-liveAlone/${episode.ep}/${filename}`}
                 alt={`Episode ${episode.ep} - Image ${index + 1}`}
-                style={{objectFit: 'cover'}}
+                style={{ objectFit: 'cover' }}
                 fill
                 quality={100}
                 sizes="200px"
               />
             </Skeleton>
           </AspectRatio>
-          <Flex m="2" style={{justifyContent:"center", alignItems: "center"}}>
-            <Text as="div" size={{initial: '1', lg: '3'}} align="center" style={{wordBreak:"keep-all", wordWrap: "break-word"}}>
+          <Flex
+            m="2"
+            style={{ justifyContent: 'center', alignItems: 'center' }}
+          >
+            <Text
+              as="div"
+              size={{ initial: '1', lg: '3' }}
+              align="center"
+              style={{
+                wordBreak: 'keep-all',
+                wordWrap: 'break-word',
+                paddingTop: '10px',
+              }}
+            >
               {title}
             </Text>
           </Flex>
-        </Flex>
+        </Card>
       </Dialog.Trigger>
       <Dialog.Portal>
         <Overlay />
-        <Content
-          ref={containerRef}
-          aria-describedby={undefined}
-          >
-          <VisuallyHidden.Root asChild><Dialog.Title></Dialog.Title>
+        <Content ref={containerRef} aria-describedby={undefined}>
+          <VisuallyHidden.Root asChild>
+            <Dialog.Title></Dialog.Title>
           </VisuallyHidden.Root>
           <Dialog.Close asChild>
             <CloseButton aria-label="Close">
-              <Cross2Icon width="100%" height="100%"/>
+              <Cross2Icon width="100%" height="100%" />
             </CloseButton>
           </Dialog.Close>
           <Box
             style={{
               position: 'relative',
-              width: `${naturalWidth}px`, 
+              width: `${naturalWidth}px`,
               height: `${naturalHeight}px`,
-              maxWidth: "70vw",
-              maxHeight: "70vh",
-              margin: 'auto'
-            }}>
+              maxWidth: '70vw',
+              maxHeight: '70vh',
+              margin: 'auto',
+            }}
+          >
             <Skeleton loading={true}>
               <Image
                 src={`/images/tv-liveAlone/${episode.ep}/${filename}`}
                 alt={`Episode ${episode.ep} - Image ${index + 1}`}
                 style={{
-                  objectFit: 'contain'
-                  }}
+                  objectFit: 'contain',
+                }}
                 fill
                 quality={100}
                 sizes={'70vw'}
-                onLoadingComplete={(img)=>{
-                  handleImgHeight(img)}}
+                onLoadingComplete={(img) => {
+                  handleImgHeight(img);
+                }}
               />
             </Skeleton>
           </Box>
         </Content>
       </Dialog.Portal>
     </Dialog.Root>
-  )
+  );
 }
