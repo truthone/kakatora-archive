f8cfe71b13a8a10467c7e538b6a7ca7810ac4594
한정아 <32234327+truthone@users.noreply.github.com>
2024-10-24 08:27:16 +0000

    필모그래피 연도별/카테고리별 필터 진행중

--- app/filmo-by-ott/page.js
+++ app/filmo-by-ott/page.js
@@ -1,13 +1,13 @@
 'use client';
 
 import React from 'react';
-import FilmoFilter from '../../components/FilmoFilter'
+import FilmoFilterByOTT from '../../components/FilmoFilterByOTT'
 import { Container } from '@radix-ui/themes';
 
 export default function FilmoByOTTPage({ }) {
   return (
     <Container p="4">
-      <FilmoFilter />
+      <FilmoFilterByOTT />
     </Container>
   )
 }
--- app/filmography/page.js
+++ app/filmography/page.js
@@ -1,12 +1,26 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import filmoDataByYear from '../../data/filmoDataByYear.json'
 import FilmoSection from '../../components/FilmoSection'
+import FilmoFilter from '../../components/FilmoFilter'
 import { Container } from '@radix-ui/themes';
 
 export default function FilmographyPage() {
+  const [filteredData, setFilteredData] = useState(filmoDataByYear.filmo_data_by_year);
+
+  const handleFilterChange = (year, categories) => {
+    let filtered = filmoDataByYear.filmo_data_by_year.filter((yearData) => {
+      const isYearMatch = year === '전체' || yearData.year === year;
+      const isCategoryMatch = categories.some((category) => yearData[category.toLowerCase()]);
+      return isYearMatch && isCategoryMatch;
+    });
+    setFilteredData(filtered);
+  };
+
   return (
     <Container p="4">
-      <FilmoSection data={filmoDataByYear.filmo_data_by_year} />
+      <FilmoFilter onFilterChange={handleFilterChange} />
+      <FilmoSection data={filteredData} />
     </Container>
   )
 }
