8dbc60b31408f1fad273c88b9c4a9889aa53f810
한정아 <32234327+truthone@users.noreply.github.com>
2024-10-28 07:39:29 +0000

    update: 클릭방지 타이머

--- components/FilmoSection.jsx
+++ components/FilmoSection.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, Heading, Section } from '@radix-ui/themes';
 import FilmoRow from './FilmoRow';
 
 function FilmoSection({ data }) {
+  const [isClickable, setIsClickable] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsClickable(true);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <Section size="1">
       {data.map((yearData, index) => (
-        <Box key={index} mt="6">
+        <Box key={`${index}-${yearData.id}`} mt="6">
           <Heading>{yearData?.year}</Heading>
           {yearData?.movies && yearData.movies.length > 0 && (
-            <FilmoRow title="영화" contents={yearData?.movies} />
+            <FilmoRow title="영화" contents={yearData?.movies} isClickable={isClickable} />
           )}
           {yearData?.dramas && yearData.dramas.length > 0 && (
-            <FilmoRow title="드라마" contents={yearData?.dramas} />
+            <FilmoRow title="드라마" contents={yearData?.dramas} isClickable={isClickable} />
           )}
           {yearData?.musicals && yearData.musicals.length > 0 && (
-            <FilmoRow title="연극" contents={yearData?.musicals} />
+            <FilmoRow title="연극" contents={yearData?.musicals} isClickable={isClickable} />
           )}
           {yearData?.tv_appearances && yearData.tv_appearances.length > 0 && (
-            <FilmoRow title="예능" contents={yearData?.tv_appearances} />
+            <FilmoRow title="예능" contents={yearData?.tv_appearances} isClickable={isClickable} />
           )}
         </Box>
       ))}
