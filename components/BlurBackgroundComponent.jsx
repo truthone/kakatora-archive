12f81a9c57609b354f979684e5bf65c53fd402f9
한정아 <32234327+truthone@users.noreply.github.com>
2024-10-29 12:36:02 +0000

    상세페이지 프로필 블러배경이미지 만드는중..

---
+++ components/BlurBackgroundComponent.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import Image from 'next/image';
+
+const Container = styled.div`
+  position: relative;
+  overflow: hidden;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: -10rem;
+    bottom: -10rem;
+    left: -10rem;
+    right: -10rem;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: -2px;
+    left: -1rem;
+    right: -1rem;
+    height: 25rem;
+    background-image: linear-gradient(to top, #000, rgba(0, 0, 0, 0));
+  }
+`;
+
+const BlurBackgroundImageWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  filter: blur(15px);
+  overflow: hidden;
+`;
+
+const BlurBackgroundComponent = ({ imageUrl }) => {
+  useEffect(() => {
+    console.log('imageUrl:', imageUrl);
+  }, [imageUrl]);
+
+  return (
+    <Container>
+      <BlurBackgroundImageWrapper>
+        <Image src={imageUrl} alt="Background Image" layout="fill" objectFit="cover" />
+      </BlurBackgroundImageWrapper>
+    </Container>
+  );
+};
+
+export default BlurBackgroundComponent;
