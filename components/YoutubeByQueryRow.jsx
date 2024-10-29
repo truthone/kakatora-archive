7f4baaa62c25547017cff1eb342c53ce89ec0a3e
한정아 <32234327+truthone@users.noreply.github.com>
2024-10-29 12:07:16 +0000

    update: padding, margi n

--- components/YoutubeByQueryRow.jsx
+++ components/YoutubeByQueryRow.jsx
@@ -46,47 +46,49 @@ function YouTubeByQueryRow({ SectionTitle, query }) {
       <Heading size="6" mb="4">
         {SectionTitle}
       </Heading>
-      <ScrollArrowWrapper itemWidth={300} gap={12}>
-        <Flex gap="3">
-          {validVideos.map((video) => (
-            <Card
-              key={video.id.videoId}
-              style={{ width: '300px', flexShrink: 0, cursor: 'pointer' }}
-              my="3"
-              onClick={() => openYouTubeVideo(video.id.videoId)}
-              className="item"
-            >
-              <AspectRatio ratio={16 / 9}>
-                <Image
-                  src={video.snippet.thumbnails.high.url}
-                  alt={video.snippet.title}
-                  fill
-                  sizes={'30vw'}
-                  style={{ objectFit: 'cover' }}
-                />
-              </AspectRatio>
-              <Box p="2">
-                <Text
-                  as="p"
-                  size="2"
-                  weight="bold"
-                  style={{
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis',
-                    display: '-webkit-box',
-                    WebkitLineClamp: '2',
-                    WebkitBoxOrient: 'vertical',
-                    lineHeight: '1.2em',
-                    maxHeight: '2.4em', // lineHeight * 2
-                  }}
-                >
-                  {decode(video.snippet.title)}
-                </Text>
-              </Box>
-            </Card>
-          ))}
-        </Flex>
-      </ScrollArrowWrapper>
+      <Box ml="6">
+        <ScrollArrowWrapper itemWidth={300} gap={12}>
+          <Flex gap="3">
+            {validVideos.map((video) => (
+              <Card
+                key={video.id.videoId}
+                style={{ width: '300px', flexShrink: 0, cursor: 'pointer' }}
+                my="3"
+                onClick={() => openYouTubeVideo(video.id.videoId)}
+                className="item"
+              >
+                <AspectRatio ratio={16 / 9}>
+                  <Image
+                    src={video.snippet.thumbnails.high.url}
+                    alt={video.snippet.title}
+                    fill
+                    sizes={'30vw'}
+                    style={{ objectFit: 'cover' }}
+                  />
+                </AspectRatio>
+                <Box p="2">
+                  <Text
+                    as="p"
+                    size="2"
+                    weight="bold"
+                    style={{
+                      overflow: 'hidden',
+                      textOverflow: 'ellipsis',
+                      display: '-webkit-box',
+                      WebkitLineClamp: '2',
+                      WebkitBoxOrient: 'vertical',
+                      lineHeight: '1.2em',
+                      maxHeight: '2.4em', // lineHeight * 2
+                    }}
+                  >
+                    {decode(video.snippet.title)}
+                  </Text>
+                </Box>
+              </Card>
+            ))}
+          </Flex>
+        </ScrollArrowWrapper>
+      </Box>
     </Section>
   );
 }
