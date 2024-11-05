3494807dbce6027636dcd9b0e242cb35079e5a48
Your Name <you@example.com>
2024-11-05 17:49:45 +0900

    update: footer contact

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
-import { Box, Text, Flex, Section } from '@radix-ui/themes';
+import { Link, Text, Flex, Section } from '@radix-ui/themes';
 
 function Footer() {
   return (
     <Section style={{ backgroundColor: 'var(--color-panel-solid)' }}>
-      <Flex direction="column" align="center" gap="2">
+      <Flex direction="column" justify="center" align="center" gap="2">
         <Text size="2" color="gray">
           &copy; 2024 HBD ju-seung
         </Text>
-        <Flex gap="4" wrap="wrap" justify="center">
-          <Text as="a" href="#" size="1" color="gray"></Text>
-          <Text as="a" href="#" size="1" color="gray"></Text>
-          <Text as="a" href="#" size="1" color="gray"></Text>
+        <Flex gap="4" wrap="wrap" justify="center" align="center">
+          <Text size="1" color="gray">CONTACT: </Text>
+          <Link
+            size="1"
+            color="gray"
+            underline="hover"
+            target="_blank"
+            href="https://x.com/ju___h_i__"
+          >
+            X (twitter)
+          </Link>
+          <Link
+            size="1"
+            color="gray"
+            underline="hover"
+            target="_blank"
+            href="mailto:j.dingdingdingding@gmail.com"
+          >
+            Mail
+          </Link>
         </Flex>
       </Flex>
     </Section>
