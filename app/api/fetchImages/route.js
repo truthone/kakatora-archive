bac88a788330a526df01bbc32b6ee587a81ceb8f
Your Name <you@example.com>
2024-11-08 11:20:49 +0900

    update: auth 키 조작

--- app/api/fetchImages/route.js
+++ app/api/fetchImages/route.js
@@ -18,10 +18,11 @@ export async function GET(request) {
     const auth = new google.auth.GoogleAuth({
       credentials: {
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\\n/g, '\n')
       },
       scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
     });
+
 
     const sheets = google.sheets({ version: 'v4', auth });
     const response = await sheets.spreadsheets.values.get({
