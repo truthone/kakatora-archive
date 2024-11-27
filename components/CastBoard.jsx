556879709584583bd0267749150bf6a40291757c
한정아 <32234327+truthone@users.noreply.github.com>
2024-11-27 11:36:45 +0900

    CastBoard.jsx 업데이트

--- components/CastBoard.jsx
+++ components/CastBoard.jsx
@@ -143,7 +143,7 @@ const CastBoard = () => {
             <AspectRatio ratio={2 / 3}>
               <Image
                 fill
-                src={'/tebas/GIL.jpg'}
+                src={'/tebas/SEOK.jpg'}
                 alt={'S'}
                 style={{
                   objectFit: 'cover',
@@ -165,7 +165,7 @@ const CastBoard = () => {
                 paddingBottom: '5px',
               }}
             >
-              길은성
+              이석준
             </Name>
           </Box>
           <AnimationBox2>
@@ -182,7 +182,7 @@ const CastBoard = () => {
             <AspectRatio ratio={2 / 3}>
               <Image
                 fill
-                src={'/tebas/SON.jpg'}
+                src={'/tebas/TAEK.jpg'}
                 alt={'마페'}
                 style={{
                   objectFit: 'cover',
@@ -203,7 +203,7 @@ const CastBoard = () => {
                 paddingBottom: '5px',
               }}
             >
-              손우현
+              정택운
             </Name>
           </Box>
           <AnimationBox2>
