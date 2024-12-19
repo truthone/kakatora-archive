e8b3e599c316f6f7ff6546ce2a03e8ad6abef7bf
한정아 <32234327+truthone@users.noreply.github.com>
2024-12-19 08:52:27 +0000

    add: article feature

---
+++ app/api/fetchCafeArticles/route.js
@@ -0,0 +1,83 @@
+import { NextResponse } from 'next/server';
+import { google } from 'googleapis';
+
+let blockUntil = null;
+
+export async function GET(request) {
+  const params = Object.fromEntries(request.nextUrl.searchParams.entries());
+  const { filmoId } = params;
+
+  // 타입 변환 처리
+  const parsedEpisode = episode;
+
+  if (!process.env.GOOGLE_CLIENT_EMAIL || !process.env.GOOGLE_PRIVATE_KEY || !process.env.GOOGLE_SHEET_ID) {
+    return NextResponse.json(
+      { error: 'Environment variables are not properly configured' },
+      { status: 500 }
+    );
+  }
+
+  if (blockUntil && new Date() < blockUntil) {
+    return NextResponse.json(
+      { error: 'API temporarily blocked due to previous error' },
+      { status: 429 }
+    );
+  }
+
+  try {
+    const auth = new google.auth.GoogleAuth({
+      credentials: {
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\\\n/g, '\n'),
+      },
+      scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+    });
+
+    const sheets = google.sheets({ version: 'v4', auth });
+    const sheetName = 'cafeArticles';
+    // 데이터 범위 계산
+    const startCol = 'A';
+    const endCol = 'H';
+    const range = `${sheetName}!${startCol}2:${endCol}`;
+
+    // Google Sheets API 호출
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId: process.env.GOOGLE_SHEET_ID,
+      range,
+    });
+
+    const rows = response.data.values;
+    if (!rows || rows.length === 0) {
+      return NextResponse.json({ message: 'No data found' }, { status: 404 });
+    }
+
+    // 데이터 변환
+    const articlesData = rows.map((row) => ({
+      id: row[0],
+      fk: row[1],
+      date: row[2],
+      title: row[3],
+      link: row[4],
+      imageUrl: row[5],
+      note: row[6],
+      era: row[7]
+    }));
+
+    const filteredData = articlesData.filter((item) => {
+      if (filmoId) {
+        return String(item.fk) === String(filmoId);
+      }
+      return true;
+    });
+    return NextResponse.json(filteredData);
+  
+  } catch (error) {
+    console.error('Error fetching data from Google Sheets:', error);
+
+    if (error.code && error.code >= 400 && error.code < 600) {
+      blockUntil = new Date(new Date().getTime() + 5 * 60 * 1000);
+    }
+
+    return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
+  }
+}
--- components/CafeArticleSection.jsx
+++ components/CafeArticleSection.jsx
@@ -3,58 +3,15 @@ import {
   Heading,
   Section, Flex, Box, Card, Avatar, Text
 } from '@radix-ui/themes';
-import ArticleCard from './ArticleCard';
 import SpriteAnimation from './SpriteAnimation';
+import useFetchCafeArticles from '../hooks/useFetchCafeArticles';
 
 const CafeArticleSection = ({ filmoId }) => {
-  // const { articles, loading, error } = useFetchArticles({ filmoId });
-
+  const { articles, loading, error } = useFetchCafeArticles({ filmoId });
+  const fallbackTexts = ["이","주","승","승","장","구","❤️","🩷","🧡","💛","💚","💙","🤍","🩶","🖤","🤎","💜","🩵","💖","💗","💓","💝","❤️‍🔥","💕","💘"];
   return (
-    // articles.length >= 0 ? (
-    //   <Section>
-    //     <Heading size="6" mb="4">
-    //       관련 카페글
-    //     </Heading>
-    //     {
-    //       <Flex
-    //         my="4"
-    //         p="0"
-    //         direction={{ initial: 'column' }}
-    //         wrap="wrap"
-    //         gap="4"
-    //         width="100%"
-    //         justify={{ initial: 'center' }}
-    //       >
-    //         {
-    //           articles.map((article, index) => (
-    //             <Box maxWidth="240px" key={index}>
-    //               <Card>
-    //                 <Flex gap="3" align="center">
-    //                   <Avatar
-    //                     size="3"
-    //                     src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
-    //                     radius="large"
-    //                     fallback="이주승승장구"
-    //                   />
-    //                   <Box>
-    //                     <Text as="div" size="2" weight="bold">
-    //                       [직찍] 시사회 현장 담아왔어요!
-    //                     </Text>
-    //                     <Text as="div" size="2" color="gray">
-    //                       24.06.12
-    //                     </Text>
-    //                   </Box>
-    //                 </Flex>
-    //               </Card>
-    //             </Box>
-
-    //           ))
-    //         }
-    //       </Flex>
-    //     }
-    //   </Section>
-    // ) : null
-    <Section>
+    articles.length >= 0 ? (
+      <Section>
         <Heading size="6" mb="4">
           관련 카페글
         </Heading>
@@ -67,70 +24,43 @@ const CafeArticleSection = ({ filmoId }) => {
             gap="4"
             width="100%"
             justify={{ initial: 'center' }}
+            style={{ cursor: 'pointer' }}
           >
-            <Box maxWidth="640px" key={1}>
+            {
+              articles.map((article, index) => (
+                <Box maxWidth="640px" key={index} 
+                onClick={()=>{
+                  window.open(
+                    `${article.link}`,
+                    '_blank',
+                    'noopener,noreferrer'
+                  );
+                }}>
                   <Card>
                     <Flex gap="3" align="center">
                       <Avatar
                         size="3"
-                        src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+                        src=""
                         radius="large"
-                        fallback="이주승승장구"
+                        fallback={fallbackTexts[index % fallbackTexts.length]}
                       />
                       <Box>
                         <Text as="div" size="2" weight="bold">
-                          [직찍] 시사회 현장 담아왔어요!
+                          {article.title}
                         </Text>
                         <Text as="div" size="2" color="gray">
-                          24.06.12
+                          {article.date}
                         </Text>
                       </Box>
                     </Flex>
                   </Card>
                 </Box>
-                <Box maxWidth="640px" key={2}>
-                  <Card>
-                    <Flex gap="3" align="center">
-                      <Avatar
-                        size="3"
-                        src="htps://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
-                        radius="large"
-                        fallback=""
-                      />
-                      <Box>
-                        <Text as="div" size="2" weight="bold">
-                          [직찍] 시사회 현장 담아왔어요!
-                        </Text>
-                        <Text as="div" size="2" color="gray">
-                          24.06.12
-                        </Text>
-                      </Box>
-                    </Flex>
-                  </Card>
-                </Box>
-                <Box maxWidth="640px" key={3}>
-                  <Card>
-                    <Flex gap="3" align="center">
-                      <Avatar
-                        size="3"
-                        src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
-                        radius="large"
-                        fallback="이주승승장구"
-                      />
-                      <Box>
-                        <Text as="div" size="2" weight="bold">
-                          [직찍] 시사회 현장 담아왔어요!
-                        </Text>
-                        <Text as="div" size="2" color="gray">
-                          24.06.12
-                        </Text>
-                      </Box>
-                    </Flex>
-                  </Card>
-                </Box>
+              ))
+            }
           </Flex>
         }
       </Section>
+    ) : null
   );
 };
 export default CafeArticleSection;
