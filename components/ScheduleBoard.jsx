492f7bfee479eb84a25bf6545e04447202c2231e
한정아 <32234327+truthone@users.noreply.github.com>
2025-01-03 05:13:06 +0000

    💄 : 스케줄 보드 pc 버전

--- components/ScheduleBoard.jsx
+++ components/ScheduleBoard.jsx
@@ -4,6 +4,7 @@ import {
   Flex,
   Heading,
   Text,
+  Separator,
   Table,
   Button,
   Box,
@@ -91,37 +92,42 @@ const ScheduleBoard = () => {
 
   return (
     <Flex
-      m={{ initial: '5px', sm: '80px', xs: '100px' }}
-      direction={{initial: 'column', md:'row'}}
-      style={{justifyContent:"space-evenly", }}
-      width="100%"
-      maxWidth="1000px"
+      m={{ initial: '20px', sm: '80px', xs: '100px' }}
+      direction={{ initial: 'column', md: 'row' }}
+      gap="5"
+      style={{justifyContent:"space-evenly"}}
     >
+      {/*필터그룹*/}
       <Flex
+        height="fit-content"
         direction="column"
+        flexShrink="0"
+        minWidth="320px"
+        width={{initial:'100%', md:'fit-content'}}
         p="3"
         mb="6"
-        height="fit-content"
-        width={{initial: '100%', md:'fit-content'}}
+        pt="6"
         style={{
-          justifyContent: 'flex-start',
+          justifyContent: 'center',
           alignItems: 'center',
           boxSizing: 'border-box',
-
+          position: 'sticky',
+          top: '60px',
           backgroundColor: 'var(--gray-1)',
+          zIndex: 100,
         }}
       >
         <Heading
-          my="4"
           align="center"
           size={{ initial: '5', xs: '8' }}
           style={{ color: 'rgba(175,25,27,255)', fontFamily: 'SBAggroB' }}
         >
           SCHEDULE
         </Heading>
+        <Separator size="4" my={{ initial: '2', xs: '4' }} />
 
         {/* 필터 그룹 */}
-        <Box style={{}}>
+        <Box>
           <FilterGroup
             title="요일"
             options={daysOptions}
@@ -142,155 +148,166 @@ const ScheduleBoard = () => {
           />
         </Box>
       </Flex>
-      {/* 지난 스케줄 */}
-      <Box>
-      <Flex
-        direction="column"
-        my="4"
-        width="100%"
-        px="2"
-        py="3"
-        style={{ backgroundColor: 'var(--gray-3)', 
-          borderRadius: '10px', }}
-      >
-        <Flex justify="space-between" align="center" mb="2" py="4"width="100%" >
-          <Text size="4" mr="2">
-            지난 스케줄
-          </Text>
-          <Button
-            variant="ghost"
-            style={{cursor: 'pointer'}}
-            onClick={() => setShowPastSchedules(!showPastSchedules)}
-          >
-            {showPastSchedules ? '▲' : '▼'}
-          </Button>
-        </Flex>
-        {showPastSchedules && (
-        <ScrollArea scrollbars="vertical" 
-          style={{ maxHeight: '450px' , display:'table'}}
-          >
-          <Table.Root
-            variant="surface"width="fit-content"style={{margin:"0 auto", position:"relative"}}
-          >
-              <Table.Header
-                align="center"
-                style={{
-                  position: 'sticky',
-                  top: 0,
-                  zIndex: 100,
-                  backgroundColor: 'var(--gray-2)',
-                }}
-              >
-                <Table.Row align="center">
-                  <Table.ColumnHeaderCell>날짜</Table.ColumnHeaderCell>
-                  <Table.ColumnHeaderCell>시간</Table.ColumnHeaderCell>
-                  <Table.ColumnHeaderCell>S</Table.ColumnHeaderCell>
-                  <Table.ColumnHeaderCell minWidth="80px">
-                    마르틴
-                    <br />&<br />
-                    페데리코
-                  </Table.ColumnHeaderCell>
-                </Table.Row>
-              </Table.Header>
-              
-              <Table.Body align="center">
+      <Flex direction="column" width="100%" maxWidth="800px">
+        {/* 지난 스케줄 */}
 
-                {filteredPastSchedules.map((s) => (
-                  <Table.Row key={s.date + s.time} align="center">
-                    <Table.RowHeaderCell>
-                      {`${s.date} ${s.day}`}
-                      {s?.note && (
-                        <Text mt="1" style={{ fontStyle: 'italic' }}>
-                          <br /> {s.note}
-                        </Text>
-                      )}
-                    </Table.RowHeaderCell>
-                    <Table.Cell>
-                      {s?.note !== '공연없음' ? <>{s.time}</> : null}
-                    </Table.Cell>
-                    <Table.Cell>{s.akaS}</Table.Cell>
-                    <Table.Cell>{s.martin}</Table.Cell>
-                  </Table.Row>
-                ))
-                } 
-              </Table.Body>
-          </Table.Root>
-          </ScrollArea>
-        )}
-      </Flex>
-
-      {/* 다가오는 스케줄 */}
-      <Flex
-        direction="column"
-        width="100%"
-        my="4"
-        px="2"
-        py="3"
-        style={{
-          backgroundColor: 'var(--gray-3)',
-          borderRadius: '10px',
-        }}
-      >
-        <Flex justify="space-between" align="center" mb="2" py="4" width="100%">
-          <Text size="4" mr="2">
-            다가오는 스케줄
-          </Text>
-          <Button
-            variant="ghost"
-            
-            style={{cursor: 'pointer'}}
-            onClick={() => setShowUpcomingSchedules(!showUpcomingSchedules)}
+        <Flex
+          direction="column"
+          my="4"
+          width="100%"
+          px="2"
+          py="3"
+          style={{ backgroundColor: 'var(--gray-3)', borderRadius: '10px' }}
+        >
+          <Flex
+            justify="space-between"
+            align="center"
+            mb="2"
+            py="4"
+            width="100%"
           >
-            {showUpcomingSchedules ? '▲' : '▼'}
-          </Button>
-        </Flex>
-        {showUpcomingSchedules && (
-          <Table.Root variant="surface" style={{width:"fit-content",margin: "0 auto"}}>
-            <ScrollArea scrollbars="vertical" style={{ maxHeight: '450px' }}>
-              <Table.Header
-                align="center"
-                style={{
-                  position: 'sticky',
-                  top: 0,
-                  zIndex: 100,
-                  backgroundColor: 'var(--gray-2)',
-                }}
-              >
-                <Table.Row align="center">
-                  <Table.ColumnHeaderCell>날짜</Table.ColumnHeaderCell>
-                  <Table.ColumnHeaderCell>시간</Table.ColumnHeaderCell>
-                  <Table.ColumnHeaderCell>S</Table.ColumnHeaderCell>
-                  <Table.ColumnHeaderCell style={{minWidth:'80px'}}>
-                    마르틴
-                    <br />&<br />
-                    페데리코
-                  </Table.ColumnHeaderCell>
-                </Table.Row>
-              </Table.Header>
-              <Table.Body align="center">
-                {filteredUpcomingSchedules.map((s) => (
-                  <Table.Row key={s.date + s.time} align="center">
-                    <Table.RowHeaderCell>
-                      {`${s.date} ${s.day}`}
-                      {s?.note && (
-                        <Text mt="1" style={{ fontStyle: 'italic' }}>
-                          <br /> {s.note}
-                        </Text>
-                      )}
-                    </Table.RowHeaderCell>
-                    <Table.Cell>
-                      {s?.note !== '공연없음' ? <>{s.time}</> : null}
-                    </Table.Cell>
-                    <Table.Cell>{s.akaS}</Table.Cell>
-                    <Table.Cell>{s.martin}</Table.Cell>
+            <Text size="4" mr="2">
+              지난 스케줄
+            </Text>
+            <Button
+              variant="ghost"
+              style={{ cursor: 'pointer' }}
+              onClick={() => setShowPastSchedules(!showPastSchedules)}
+            >
+              {showPastSchedules ? '▲' : '▼'}
+            </Button>
+          </Flex>
+          {showPastSchedules && (
+            <Table.Root
+              variant="surface"
+              width="fit-content"
+              style={{ margin: '0 auto' }}
+            >
+              <ScrollArea scrollbars="vertical" style={{ maxHeight: '450px' }}>
+                <Table.Header
+                  align="center"
+                  style={{
+                    position: 'sticky',
+                    top: 0,
+                    zIndex: 100,
+                    backgroundColor: 'var(--gray-2)',
+                  }}
+                >
+                  <Table.Row align="center">
+                    <Table.ColumnHeaderCell>날짜</Table.ColumnHeaderCell>
+                    <Table.ColumnHeaderCell>시간</Table.ColumnHeaderCell>
+                    <Table.ColumnHeaderCell>S</Table.ColumnHeaderCell>
+                    <Table.ColumnHeaderCell minWidth="80px">
+                      마르틴
+                      <br />&<br />
+                      페데리코
+                    </Table.ColumnHeaderCell>
                   </Table.Row>
-                ))}
-              </Table.Body>
-            </ScrollArea>
-          </Table.Root>
-        )}
+                </Table.Header>
+                <Table.Body align="center">
+                  {filteredPastSchedules.map((s) => (
+                    <Table.Row key={s.date + s.time} align="center">
+                      <Table.RowHeaderCell>
+                        {`${s.date} ${s.day}`}
+                        {s?.note && (
+                          <Text mt="1" style={{ fontStyle: 'italic' }}>
+                            <br /> {s.note}
+                          </Text>
+                        )}
+                      </Table.RowHeaderCell>
+                      <Table.Cell>
+                        {s?.note !== '공연없음' ? <>{s.time}</> : null}
+                      </Table.Cell>
+                      <Table.Cell>{s.akaS}</Table.Cell>
+                      <Table.Cell>{s.martin}</Table.Cell>
+                    </Table.Row>
+                  ))}
+                </Table.Body>
+              </ScrollArea>
+            </Table.Root>
+          )}
+        </Flex>
+
+        {/* 다가오는 스케줄 */}
+        <Flex
+          direction="column"
+          width="100%"
+          my="4"
+          px="2"
+          py="3"
+          style={{
+            backgroundColor: 'var(--gray-3)',
+            borderRadius: '10px',
+          }}
+        >
+          <Flex
+            justify="space-between"
+            align="center"
+            mb="2"
+            py="4"
+            width="100%"
+          >
+            <Text size="4" mr="2">
+              다가오는 스케줄
+            </Text>
+            <Button
+              variant="ghost"
+              style={{ cursor: 'pointer' }}
+              onClick={() => setShowUpcomingSchedules(!showUpcomingSchedules)}
+            >
+              {showUpcomingSchedules ? '▲' : '▼'}
+            </Button>
+          </Flex>
+          {showUpcomingSchedules && (
+            <Table.Root
+              variant="surface"
+              style={{ width: 'fit-content', margin: '0 auto' }}
+            >
+              <ScrollArea scrollbars="vertical" style={{ maxHeight: '450px' }}>
+                <Table.Header
+                  align="center"
+                  style={{
+                    position: 'sticky',
+                    top: 0,
+                    zIndex: 100,
+                    backgroundColor: 'var(--gray-2)',
+                  }}
+                >
+                  <Table.Row align="center">
+                    <Table.ColumnHeaderCell>날짜</Table.ColumnHeaderCell>
+                    <Table.ColumnHeaderCell>시간</Table.ColumnHeaderCell>
+                    <Table.ColumnHeaderCell>S</Table.ColumnHeaderCell>
+                    <Table.ColumnHeaderCell style={{ minWidth: '80px' }}>
+                      마르틴
+                      <br />&<br />
+                      페데리코
+                    </Table.ColumnHeaderCell>
+                  </Table.Row>
+                </Table.Header>
+                <Table.Body align="center">
+                  {filteredUpcomingSchedules.map((s) => (
+                    <Table.Row key={s.date + s.time} align="center">
+                      <Table.RowHeaderCell>
+                        {`${s.date} ${s.day}`}
+                        {s?.note && (
+                          <Text mt="1" style={{ fontStyle: 'italic' }}>
+                            <br /> {s.note}
+                          </Text>
+                        )}
+                      </Table.RowHeaderCell>
+                      <Table.Cell>
+                        {s?.note !== '공연없음' ? <>{s.time}</> : null}
+                      </Table.Cell>
+                      <Table.Cell>{s.akaS}</Table.Cell>
+                      <Table.Cell>{s.martin}</Table.Cell>
+                    </Table.Row>
+                  ))}
+                </Table.Body>
+              </ScrollArea>
+            </Table.Root>
+          )}
+        </Flex>
       </Flex>
-      </Box>
     </Flex>
   );
 };
