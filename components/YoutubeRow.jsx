cfa6c3df77ac3c879d54ffe2e1bee901b256f686
ja.han <truth.h406@gmail.com>
2025-03-25 02:52:17 +0900

    fix: 유튜브 재생목록 내 비공개 또는 삭제 동영상 렌더링 제외

--- components/YoutubeRow.jsx
+++ components/YoutubeRow.jsx
@@ -58,56 +58,62 @@ function YoutubeRow({ sectionTitle, playlistId }) {
       message="loading"
     />
   ) : (
-    <Section size="1">
-      <Heading size="6" mb="4">
-        {sectionTitle || '관련 영상'}
-      </Heading>
-      <Box ml={{ initial: '1', xs: '4' }}>
-        <ScrollArrowWrapper itemWidth={300} gap={12}>
-          <Flex gap="3">
-            {videos.map((video) => (
-              <Card
-                key={video.snippet.resourceId.videoId}
-                style={{ width: '300px', flexShrink: 0, cursor: 'pointer' }}
-                my="3"
-                onClick={() =>
-                  openYouTubeVideo(video.snippet.resourceId.videoId)
-                }
-                className="item"
-              >
-                <AspectRatio ratio={16 / 9}>
-                  <Image
-                    src={video.snippet.thumbnails.high.url}
-                    alt={video.snippet.title}
-                    fill
-                    sizes={'30vw'}
-                    style={{ objectFit: 'cover' }}
-                  />
-                </AspectRatio>
-                <Box p="2">
-                  <Text
-                    as="p"
-                    size="2"
-                    weight="bold"
-                    style={{
-                      overflow: 'hidden',
-                      textOverflow: 'ellipsis',
-                      display: '-webkit-box',
-                      WebkitLineClamp: '2',
-                      WebkitBoxOrient: 'vertical',
-                      lineHeight: '1.2em',
-                      maxHeight: '2.4em', // lineHeight * 2
-                    }}
-                  >
-                    {decode(video.snippet.title)}
-                  </Text>
-                </Box>
-              </Card>
-            ))}
-          </Flex>
-        </ScrollArrowWrapper>
-      </Box>
-    </Section>
+<Section size="1">
+  <Heading size="6" mb="4">
+    {sectionTitle || '관련 영상'}
+  </Heading>
+  <Box ml={{ initial: '1', xs: '4' }}>
+    <ScrollArrowWrapper itemWidth={300} gap={12}>
+      <Flex gap="3">
+        {videos
+          .filter(video => 
+            video.snippet.title !== "Deleted video" &&
+            video.snippet.title !== "Private video" &&
+            Object.keys(video.snippet.thumbnails || {}).length > 0
+          )
+          .map(video => (
+            <Card
+              key={video.snippet.resourceId.videoId}
+              style={{ width: '300px', flexShrink: 0, cursor: 'pointer' }}
+              my="3"
+              onClick={() => openYouTubeVideo(video.snippet.resourceId.videoId)}
+              className="item"
+            >
+              <AspectRatio ratio={16 / 9}>
+                <Image
+                  src={video?.snippet?.thumbnails?.high?.url}
+                  alt={video.snippet.title}
+                  fill
+                  sizes={'30vw'}
+                  style={{ objectFit: 'cover' }}
+                />
+              </AspectRatio>
+              <Box p="2">
+                <Text
+                  as="p"
+                  size="2"
+                  weight="bold"
+                  style={{
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                    display: '-webkit-box',
+                    WebkitLineClamp: '2',
+                    WebkitBoxOrient: 'vertical',
+                    lineHeight: '1.2em',
+                    maxHeight: '2.4em',
+                  }}
+                >
+                  {decode(video.snippet.title)}
+                </Text>
+              </Box>
+            </Card>
+          ))
+        }
+      </Flex>
+    </ScrollArrowWrapper>
+  </Box>
+</Section>
+
   );
 }
 
