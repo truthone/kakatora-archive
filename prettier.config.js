18c8a5dc9a95cbfd045578bcce9741d7fd74b631
ja.han <truth.h406@gmail.com>
2025-05-06 01:42:03 +0900

    🔧 update: config

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+// prettier.config.js
+/** @type {import('prettier').Config} */
+export default {
+  singleQuote: true,
+  semi: true,
+  printWidth: 100,
+  tabWidth: 2,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+};
